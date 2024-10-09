142f7dc402f50ce467a2524c6f43bd3055c406b4
Nikoleta <141028635+Nikoleta79@users.noreply.github.com>
2024-10-09 15:40:50 +0200

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <img alt="Real Estate Care Logo" src="./assets/Text.png">
+    <HelloWorld msg="Hello World en Welcome to Real Estate Care!" />
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  text-align: center;
+  color: rgba(71, 94, 108, 1); 
+  margin-top: 60px;
+  background-color: rgba(0, 170, 162, 1); 
+}
+</style>
+
+
